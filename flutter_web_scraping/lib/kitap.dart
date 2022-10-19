9c58c86b1237a9b4720d806f297f81de6cc3147d
sebahattan <98536025+sebahattan@users.noreply.github.com>
2022-10-19 17:46:46 +0300

    WebScraping
    İnternet Sayfasından veri çekme işlemleri

---
+++ flutter_web_scraping/lib/kitap.dart
@@ -0,0 +1,16 @@
+class Kitap {
+  String image;
+  String kitapAdi;
+  String yayinEvi;
+  String yazar;
+  String fiyat;
+  Kitap({
+    required this.image,
+    required this.kitapAdi,
+    required this.yayinEvi,
+    required this.yazar,
+    required this.fiyat 
+
+
+  });
+}
